b850df9bbfeedc441ef008523060f53b116ac885
rekindle402 <rekindle402@gmail.com>
2025-09-11 17:30:47 +0900

    workflow test

--- src/main/java/com/eneifour/fantry/common/controller/HealthCheckController.java
+++ src/main/java/com/eneifour/fantry/common/controller/HealthCheckController.java
@@ -13,6 +13,7 @@ public class HealthCheckController {
 
     // 테스트용 주석입니다
     // 진짜 찐막
+    // 찐찐막
     @GetMapping("/health")
     public ResponseEntity<Map<String, String>> healthCheck() {
         // 간단한 상태 메시지와 현재 시간을 담은 Map 객체를 생성합니다.
