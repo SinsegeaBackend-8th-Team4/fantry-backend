9d76ee60ad87599cd80ce73161099861614f17ba
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2025-09-24 12:09:17 +0900

    Add mock JavaMailSender to FantryApplicationTests

--- src/test/java/com/eneifour/fantry/FantryApplicationTests.java
+++ src/test/java/com/eneifour/fantry/FantryApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FantryApplicationTests {
+    @MockBean
+    private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
