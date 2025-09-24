787f84307ba8b8b87be813fb16ea90ddc3858c25
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2025-09-24 12:19:38 +0900

    Fix/properties (#11)
    * fix: modify Project properties config
    
    * cofig: add email test properties
    
    * fix: modify h2 test dependency
    
    * add test code

--- src/test/java/com/eneifour/fantry/FantryApplicationTests.java
+++ src/test/java/com/eneifour/fantry/FantryApplicationTests.java
@@ -2,14 +2,18 @@ package com.eneifour.fantry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class FantryApplicationTests {
     @MockBean
     private JavaMailSender javaMailSender;
 
+    @MockBean
+    private JavaMailSender javaMailSender;
+
     @Test
     void contextLoads() {
     }
-
 }
